7a7f96a1447c8e9279ab3797c6d7d6488bcce6aa
Jim Rosser <jarosser06@gmail.com>
2012-12-14 10:28:21 -0600

    global variables used correctly, fixed typos, changed spacing, added requires syslinux package for pxelinux.0 and menu.c32

--- manifests/menu.pp
+++ manifests/menu.pp
@@ -1,7 +1,7 @@
 class pxe::menu { 
   include concat::setup
-  $tftp_root = $::pxe::tftp_root
-  $menufile="${tftp_root}/pxelinux.cfg/default'
+  $tftp_root = $::pxe::tftp_root 
+  $menufile  = "${tftp_root}/pxelinux.cfg/default"
  
   file { $tftp_root:
     ensure   => directory,
@@ -24,7 +24,7 @@ class pxe::menu {
     group    => admin,
     mode     => 775,
     source   => '/usr/lib/syslinux/pxelinux.0',
-    require  => File['/var/ftpd'],
+    require  => [ File[$tftp_root], Package['syslinux'] ],
   }
 
   file { "${tftp_root}/pxelinux.cfg/menu.c32":
@@ -33,7 +33,7 @@ class pxe::menu {
     group    => admin,
     mode     => 775,
     source   => '/usr/lib/syslinux/menu.c32',
-    require  => File["${tftp_root}/pxelinux.cfg"],
+    require  => [ File["${tftp_root}/pxelinux.cfg"], Package['syslinux'] ],
   }
 
   concat { $menufile:
@@ -41,28 +41,29 @@ class pxe::menu {
     group    => root,
     mode     => 644,
   }
+
 } 
 
 define pxe::menu::header (
-  $title   = "PXE Boot Menu",
+  $menu_title   = "PXE Boot Menu",
   $menu_timeout = "1000",
-  $order   = 01,
+  $order        = 01,
 ) { 
   concat::fragment { "menu_header":
-    target    => "${::pxe::tftp_root}/pxelinux.cfg/default",
+    target    => "$::pxe::tftp_root/pxelinux.cfg/default",
     order     => $order, 
     content   => template("pxe/pxe_menu_header.erb"),
   }
 } 
 
-define pxe::menu::item (
+define pxe::menu::host (
   $label,
   $kernel,
-  $append,
-  $order = 10,
+  $append     ='',
+  $order      =10,
 ) {
   concat::fragment { "menu_item_${label}":
-    target    => "${::pxe::tftp_root}/pxelinux.cfg/default",
+    target    => "$::pxe::tftp_root/pxelinux.cfg/default",
     order     => $order,
     content   => template("pxe/pxe_menu_host.erb"),
   }
