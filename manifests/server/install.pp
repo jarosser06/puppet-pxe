63c3c3833183a39260e5a26e17775b5f4e572980
Jim Rosser <jarosser06@gmail.com>
2012-12-14 12:07:04 -0600

    added extra package needed for fedora

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -1,13 +1,19 @@
 class pxe::server::install {
-  package { 'syslinux':
-    ensure   => present,
-  }
-
   package { 'dnsmasq':
     ensure   => present,
   }
 
   package { 'lighttpd':
     ensure   => present,
+  }
+
+  package { 'syslinux':
+    ensure   => present,
+  } 
+
+  if $operatingsystem == 'fedora' {
+    package { 'syslinux-tftpboot':
+      ensure  => present,
+    }
   }
 }
