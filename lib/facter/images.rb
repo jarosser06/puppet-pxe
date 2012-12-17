826234c42bac4e0a4fe612b9b0175498e3f1d067
Jim Rosser <jarosser06@gmail.com>
2012-12-17 12:02:10 -0600

    reworked images fact

--- lib/facter/images.rb
+++ lib/facter/images.rb
@@ -1,13 +1,12 @@
-base_dir = "/var/tftp"
-images = { "ubuntu_1204_64_netboot" => "#{base_dir}/ubuntu_1204_64_netboot",   "ubuntu_1204_32_netboot" => "#{base_dir}/ubuntu_1204_32_netboot"}
+base_dir = "/srv/tftp"
 
-images.each.do |name, location|
-   Facter.add(name) do
+images["ubuntu_precise_amd64", "ubuntu_precise_i386", "ubuntu_lucid_amd64", "ubuntu_lucid_i386"]
+
+images.each.do |name|
+   Facter.add("#{name}_netboot") do
       setcode do
-         if File.exist? location
-            true
-         else
-            false
+         if File.exist? "#{base_dir}/#{name}" 
+            "#{base_dir}/#{name}" 
          end
       end
    end
