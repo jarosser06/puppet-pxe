1058bdd0555298681796906a7b60f9371b04c6e7
Jim Rosser <jarosser06@gmail.com>
2012-12-17 12:00:45 -0600

    added a function to install images

---
+++ manifests/image/install.pp
@@ -0,0 +1,39 @@
+define pxe::image::install(
+  $os_name,
+  $os_version,
+  $os_arch,
+
+) {
+
+  $tmp_dir = "/tmp/${os_name}_${os_version}_${os_arch}"
+  $final_dir = "$::pxe::tftp_root/${os_name}_${os_version}_${os_arch}"
+
+  case $os_name {
+    ubuntu: {
+      $url = "http://archive.ubuntu.com/ubuntu/dists/${os_version}/main/installer-${os_arch}/current/images/netboot/netboot.tar.gz"
+    }
+    default: { fail("Unrecognized operating system for netboot installer") }
+  }
+
+  exec{ "wget $uri":
+    command => "/usr/bin/wget -q '$url' -P $tmp_dir ",
+    timeout => $timeout,
+  }
+
+  exec { "unzip netboot":
+    command => "/bin/tar -xf $tmp_dir/netboot.tar.gz",
+    cwd     => $tmp_dir,
+    timeout => 30,
+    require => Exec["wget $uri"],
+  }
+
+  file { $final_dir:
+    ensure  => directory,
+    recurse => true,
+    owner   => root,
+    group   => root,
+    mode    => 575,
+    source  => "${tmp_dir}/ubuntu-installer",
+    require => Exec["unzip netboot"],
+  }
+}
