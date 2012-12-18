dbbeeb90aca7183b89327176dbe03b31f5e4f9fb
Jim Rosser <jarosser06@gmail.com>
2012-12-18 09:18:39 -0600

    updated fact to show none if the directory doesn't exist

--- lib/facter/netboot_images.rb
+++ lib/facter/netboot_images.rb
@@ -2,6 +2,10 @@ netboot_dir = "/srv/tftp/netboot"
 
 Facter.add("netboot_images") do
   setcode do
-    Dir.entries(netboot_dir).grep(/^[a-zA-Z0-9_]*$/).join(",")
+    if File.directory?(netboot_dir)
+      Dir.entries(netboot_dir).grep(/^[a-zA-Z0-9_]*$/).join(",")
+    else
+      'none'
+    end
   end
 end
