81aafa34177c4728b1924c44f63bef8db06477bb
Jim Rosser <jarosser06@gmail.com>
2012-12-19 14:56:35 -0600

    ensure root dir is there

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
 class pxe ($tftp_root='/srv/tftp') {
+
+  file { $tftp_root :
+    ensure   => directory,
+    mode     => 775,
+    user     => 'root',
+    group    => 'admin',
+  }
+ 
   include pxe::server
   include pxe::menu  
   include pxe::dns
