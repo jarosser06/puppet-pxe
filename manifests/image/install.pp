ec48f6e05d3560faa889e88a89f219729c6f2cd5
Jim Rosser <jarosser06@gmail.com>
2012-12-19 14:47:20 -0600

    updated wget path information

--- manifests/image/install.pp
+++ manifests/image/install.pp
@@ -4,24 +4,33 @@ define pxe::image::install(
   $os_arch,
   $final_dir = "$::pxe::tftp_root/netboot/${os_name}_${os_version}_${os_arch}"
 
-
 ) {
+
+  case $operatingsystem {
+    ubuntu: {
+      if $operatingsystemrelease <= 10.04 {
+        $wget_path = "/bin/"
+      } else {
+        $wget_path = "/usr/bin/"
+      }
+    }
+  } 
 
   $tmp_dir = "/tmp/${os_name}_${os_version}_${os_arch}"
 
   case $os_name {
     ubuntu: {
-      $url = "http://archive.ubuntu.com/ubuntu/dists/${os_version}/main/installer-${os_arch}/current/images/netboot/netboot.tar.gz"
+      $uri = "http://archive.ubuntu.com/ubuntu/dists/${os_version}/main/installer-${os_arch}/current/images/netboot/netboot.tar.gz"
     }
     default: { fail("Unrecognized operating system for netboot installer") }
   }
 
   exec{ "wget $uri":
-    command => "/bin/wget -q '$url' -P $tmp_dir ",
+    command => "${wget_path}wget -q '$url' -P $tmp_dir ",
     timeout => $timeout,
   }
 
-  exec { "unzip netboot":
+  exec { "unzip $uri":
     command => "/bin/tar -xf $tmp_dir/netboot.tar.gz",
     cwd     => $tmp_dir,
     timeout => 30,
@@ -35,6 +44,6 @@ define pxe::image::install(
     group   => root,
     mode    => 575,
     source  => "${tmp_dir}/ubuntu-installer",
-    require => Exec["unzip netboot"],
+    require => Exec["unzip ${uri}"],
   }
 }
